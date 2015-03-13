355981edacc630b026dca10f7e74977c626ea295
tommy <bvone@ymail.com>
2015-03-13 13:33:05 -0700

    add test 1 pass

---
+++ src/RepeatCountGenerator.php
@@ -0,0 +1,22 @@
+<?php
+
+    class RepeatCountGenerator
+    {
+        function makeRepeatCount($input_word, $input_sentence)
+        {
+            {
+            $lowecase_user_inpute = strtolower($input_word);
+            $lowercase_sentence = strtolower($input_sentence);
+            $userinput_array = explode(" ", $lowercase_sentence);
+            $count= 0;
+
+            foreach ($userinput_array as $users)
+            if ($lowecase_user_inpute == $users) {
+                $count+= 1;
+            }
+            return $count;
+        }
+    }
+}
+
+?>
---
+++ tests/RepeatCountGeneratorTest.php
@@ -0,0 +1,103 @@
+<?php
+
+    require_once "src/RepeatCountGenerator.php";
+
+    class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
+    {
+
+        function test_makeCount_oneletter()
+        {
+            //Arrange
+            $test_RepeatCountGenerator = new RepeatCountGenerator;
+            $input_word = "t";
+            $input_sentence = "t";
+
+            //Act
+            $result = $test_RepeatCountGenerator->makeRepeatCount($input_word, $input_sentence);
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
+        // function test_makeCount_wordMathc()
+        // {
+        //     //Arrange
+        //     $test_RepeatCountGenerator = new RepeatCountGenerator;
+        //     $input_word = "home";
+        //     $input_sentence = "home";
+        //
+        //     //Act
+        //     $result = $test_RepeatCountGenerator->makeRepeatCount($input_word, $input_sentence);
+        //
+        //     //Assert
+        //     $this->assertEquals(1, $result);
+        // }
+        //
+        // function test_makeCount_oneWord()
+        // {
+        //     //Arrange
+        //     $test_RepeatCountGenerator = new RepeatCountGenerator;
+        //     $input_word = "home";
+        //     $input_sentence = "run";
+        //
+        //     //Act
+        //     $result = $test_RepeatCountGenerator->makeRepeatCount($input_word, $input_sentence);
+        //
+        //     //Assert
+        //     $this->assertEquals(0, $result);
+        // }
+        //
+        // function test_makeCount_word()
+        // {
+        //     //Arrange
+        //     $test_RepeatCountGenerator = new RepeatCountGenerator;
+        //     $input_word = "t";
+        //     $input_sentence = "t t t a";
+        //
+        //     //Act
+        //     $result = $test_RepeatCountGenerator->makeRepeatCount($input_word, $input_sentence);
+        //
+        //     //Assert
+        //     $this->assertEquals(3, $result);
+        // }
+        //
+        // function test_makeCount_multiple()
+        // {
+        //     //Arrange
+        //     $test_RepeatCountGenerator = new RepeatCountGenerator;
+        //     $input_word = "dog";
+        //     $input_sentence = "what kinda the dog do you got and what kinda the dog";
+        //
+        //     //Act
+        //     $result = $test_RepeatCountGenerator->makeRepeatCount($input_word, $input_sentence);
+        //
+        //     //Assert
+        //     $this->assertEquals(2, $result);
+        // }
+        //
+        //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+?>
