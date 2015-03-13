2615413104fd6308294499614a7d7c7df7f39406
tommy <bvone@ymail.com>
2015-03-13 14:24:47 -0700

    add form.twig and word_count.twig

--- app/app.php
+++ app/app.php
@@ -11,12 +11,13 @@
         return $app['twig']->render('form.twig');
     });
 
-    // $app->post("/wordcounted", function() use ($app) {
-    //     $input_words = $_POST['word'];
-    //     $input_sentence=$_POST['sentence'];
-    //     $results = new RepeatCountGenerator;
-    //     $result=$results->makeRepeatCount($input_words, $input_sentence);
-    //     return $app['twig']->render('count.twig', array('word_results' => $result));
-    // });
+    $app->post("/word_count", function() use ($app) {
+        $input_words = $_POST['word'];
+        $input_sentence=$_POST['sentence'];
+        $results = new RepeatCountGenerator;
+
+        $result_word = $results->makeRepeatCount($input_words, $input_sentence);
+        return $app['twig']->render('word_count.twig', array('word_count' => $result_word));
+    });
     return $app;
 ?>
